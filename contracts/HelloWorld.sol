b38cc75a8d231d6c760ba590c9a67613f1f99c86
Kabir Kapur <krkapur@ucsc.edu>
2021-07-12 00:02:58 -0700

    deployed

---
+++ contracts/HelloWorld.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract HelloWorld {
+  string name = "Kabir";
+  function getName() public view returns (string memory){
+    return name;
+  }
+
+  function setName(string calldata newName) external{
+    name = newName;
+  }
+}
